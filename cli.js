73dbc379203b782af37734110f50317ef231ea71
Johan Öbrink <johan.obrink@gmail.com>
2019-09-20 15:44:04 +0200

    feat: 🎸 Stop works

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const { spawn, exec } = require('child_process')
 const { address } = require('ip')
+const cc = require('camelcase')
 const argv = require('minimist')(process.argv.slice(2))
 
 const main = argv._[0]
@@ -31,25 +32,71 @@ function start () {
     detached: true
   }
 
-  const stack = spawn(command, args, options)
+  const pcs = spawn(command, args, options)
 
-  console.log(`stack pid: ${stack.pid}`)
-
-  stack.stdout.on('data', (data) => process.stdout.write(data))
-  stack.stderr.on('data', (data) => process.stderr.write(data))
-  stack.on('close', (code) => console.log(`child process exited with code ${code}`))
-  stack.on('error', (err) => {
-    console.error(err)
-    process.exit(1)
-  })
+  pcs.stdout.on('data', (data) => process.stdout.write(data))
+  pcs.stderr.on('data', (data) => process.stderr.write(data))
+  pcs.on('close', (code) => process.exit(code))
+  pcs.on('error', exitOnError)
 }
 
+const rxCliContainer = /cli_/
+
 function stop () {
-  console.log('stop')
+  exec('docker ps', (error, stdout, stderr) => {
+    exitOnError(error)
+
+    const containers = parseDockerPs(stdout)
+    const cliContainers = containers
+      .filter(c => rxCliContainer.test(c.names))
+
+    if (cliContainers.length === 0) {
+      console.log('No Egendata containers running')
+      process.exit(0)
+    }
+
+    
+    console.log(cliContainers.map(c => 'Stopping ' + c.names).join('\n'))
+    const args = ['stop'].concat(cliContainers.map(c => c.containerId))
+    const options = {
+      env: { ...process.env },
+      stdio: 'pipe',
+      cwd: __dirname,
+      detached: true
+    }
+    const pcs = spawn('docker', args, options)
+
+    pcs.stdout.on('data', (data) => process.stdout.write(data))
+    pcs.stderr.on('data', (data) => process.stderr.write(data))
+    pcs.on('close', (code) => process.exit(code))
+    pcs.on('error', exitOnError)
+  })
 }
 
 function help () {
   console.log('help')
 }
 
+function parseDockerPs (table) {
+  const rows = table
+    .split('\n')
+    .map((line) =>
+      line.split('  ').map(w => w.trim()).filter(w => w))
+    .filter((row) => row && row.length)
+    .map((row) =>
+      row.map(word => word.trim())
+    )
+  const names = rows.shift().map((word) => cc(word))
+  return rows.map((values) =>
+    values.reduce((res, val, ix) => ({
+      ...res,
+      [names[ix]]: val
+    }), {}))
+}
 
+function exitOnError (error) {
+  if (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
